20ae07463eb4beee544695a6047ba43d52fab709
strukovd <dmitryi97@yandex.ru>
2025-11-04 17:06:03 +0600

    GAZ-1637: added reading list on js

--- src/main/java/kg/gazprom/payments/readings/IssueTab.java
+++ src/main/java/kg/gazprom/payments/readings/IssueTab.java
@@ -3,10 +3,12 @@ package kg.gazprom.payments.readings;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
 import com.atlassian.jira.plugin.issuetabpanel.*;
 import com.google.common.collect.Lists;
 import kg.gazprom.payments.models.db.ReadingDTO;
+import kg.gazprom.payments.utils.fs;
 import kg.gazprom.payments.utils.log;
 import kg.gazprom.payments.utils.pg;
 
@@ -24,38 +26,28 @@ public class IssueTab extends AbstractIssueTabPanel3 {
     @Override
     public List<IssueAction> getActions(GetActionsRequest getActionsRequest) {
         try {
-            // Получаем ключ задачи
-            String issueKey = getActionsRequest.issue().getKey();
-            List<IssueAction> list = new ArrayList<>();
-
-            // Получаем список из БД
-            List<ReadingDTO> rows = pg.getReadings("100002647");
-
-            int count = 0;
-            if(rows != null) {
-				list.add(new GenericMessageAction("<table class='table-readings'><tbody><thead><tr><td>ID</td><td>Потребление</td><td>Дата</td><td>Показание</td><td>Источник</td></tr></thead>"));
-                while (rows.size() > count) {
-                    boolean isFirstItem = false;
-                    if(count == 0) {
-                        isFirstItem = true;
-                    }
-
-                    list.add( new ReadingItem( rows.get(count), isFirstItem ) );
-                    count++;
-                }
-				list.add(new GenericMessageAction("</tbody></table>"));
-            }
-            else { // Если записей в БД нету
-                String label = "<div style=\"text-align:center; margin-bottom:.8em;\">Записи не найдены!</div>";
-                Issue issue = getActionsRequest.issue();
-                CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
-
+			CustomFieldManager cfManager = ComponentAccessor.getCustomFieldManager();
+			CustomField cfAccount = cfManager.getCustomFieldObject("customfield_11916");
+			if(cfAccount == null) {
 				return Lists.newArrayList(
-					new GenericMessageAction(label)
+					new GenericMessageAction("<section id='readings-tab'>⚠\uFE0F Компонент поля customfield_11916 не найден</section>")
 				);
-            }
+			}
 
-            return list;
+			Issue issue = getActionsRequest.issue();
+			Object account = issue.getCustomFieldValue(cfAccount);
+			if(account == null) {
+				return Lists.newArrayList(
+					new GenericMessageAction("<section id='readings-tab'>\uD83E\uDEAA Лицевой счет не найден!</section>")
+				);
+			}
+
+			String code = fs.readFileAsString("template/ReadingSection.html");
+
+			String label = "<section id='readings-tab' data-account='"+account+"'></section>" + code;
+			return Lists.newArrayList(
+				new GenericMessageAction(label)
+			);
         }
         catch (Exception e) {
             log.error("В методе getActions вознакла ошибка!");
